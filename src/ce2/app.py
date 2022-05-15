920c2c6cc4b20cd8c851c87bf5473523f6d94f2b
ricardossiqueira <ricardosantossiqueira@poli.ufrj.br>
2022-05-15 01:00:26 -0300

    🧪test setup

--- src/ce2/app.py
+++ src/ce2/app.py
@@ -7,19 +7,6 @@ from ce2.classes.Resistor import Resistor
 from ce2.classes.Current import Current
 from ce2.functions.file_handler import file_handler
 
-# read file
-file, contents_split = file_handler('netlist0.txt')
-
-# remove comments
-no_comments = [line for line in contents_split if '*' not in line]
-
-# remove empty strings
-while ('' in no_comments):
-    no_comments.remove('')
-
-# split component in a list of lists
-circuit_matrix = [line.split(' ') for line in no_comments]
-
 
 # parse each component to it's own class
 def new_component(arr):
@@ -29,24 +16,41 @@ def new_component(arr):
         return Current(arr)
 
 
-# returns list of component classes
-circuit_matrix = [new_component(component) for component in circuit_matrix]
+def main(file_name):
 
-max_node_0 = max(circuit_matrix, key=attrgetter('node_0')).node_0
-max_node_1 = max(circuit_matrix, key=attrgetter('node_1')).node_1
-matrix_size = max(max_node_0, max_node_1) + 1
+    # read file
+    _, contents_split = file_handler(file_name)
 
-# init matrices
-G_matrix = Gm([matrix_size, matrix_size])
-I_matrix = Im([matrix_size, 1])
+    # remove comments
+    no_comments = [line for line in contents_split if '*' not in line]
 
-# add component to matrix using component's own stamp method
-for component in circuit_matrix:
-    if component._type == 'resistor':
-        G_matrix.add_component(component.stamp_function)
-    elif component._type == 'current':
-        I_matrix.add_component(component.stamp_function)
+    # remove empty strings
+    while ('' in no_comments):
+        no_comments.remove('')
 
-e = np.linalg.solve(G_matrix.drop_ground(), I_matrix.drop_ground())
+    # split component in a list of lists
+    circuit_matrix = [line.split(' ') for line in no_comments]
 
-print(e)
+    # returns list of component classes
+    circuit_matrix = [new_component(component) for component in circuit_matrix]
+
+    # return highest node in the netlist
+    max_node_0 = max(circuit_matrix, key=attrgetter('node_0')).node_0
+    max_node_1 = max(circuit_matrix, key=attrgetter('node_1')).node_1
+    matrix_size = max(max_node_0, max_node_1) + 1
+
+    # init matrices
+    G_matrix = Gm([matrix_size, matrix_size])
+    I_matrix = Im([matrix_size, 1])
+
+    # add component to matrix using component's own stamp method
+    for component in circuit_matrix:
+        if component._type == 'resistor':
+            G_matrix.add_component(component.stamp_function)
+        elif component._type == 'current':
+            I_matrix.add_component(component.stamp_function)
+
+    # voltage vector
+    e = np.linalg.solve(G_matrix.drop_ground(), I_matrix.drop_ground())
+
+    return e
---
+++ src/ce2/trab1ricardosiqueira.py
@@ -0,0 +1,5 @@
+from ce2.app import main as app_main
+
+
+def main(file_name):
+    return app_main(file_name)
