bd81a7b2e7105e91aa91b7a667d7205c09956e0e
ricardossiqueira <ricardosantossiqueira@poli.ufrj.br>
2022-05-22 17:13:55 -0300

    ➕ensure module is built first

--- trab1ricardosiqueira.py
+++ trab1ricardosiqueira.py
@@ -1,5 +1,14 @@
 from app.main import main as app_main
+import subprocess
+import sys
+
+
+def build_module():
+    print("Building module ce2-0.0.0...")
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "-e", "."],
+                          stdout=subprocess.DEVNULL)
 
 
 def main(file_name):
+    build_module()
     return app_main(file_name)
