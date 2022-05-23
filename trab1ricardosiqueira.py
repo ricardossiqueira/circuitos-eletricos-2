a2d998f5ad2d59e1be481214eeed3a35ffc41245
ricardossiqueira <ricardosantossiqueira@poli.ufrj.br>
2022-05-23 19:02:33 -0300

    🔧mypy error

--- trab1ricardosiqueira.py
+++ trab1ricardosiqueira.py
@@ -1,9 +1,4 @@
-import sys
-
-if 'unittest' in sys.modules.keys():
-    from app.main import main as app_main
-else:
-    from src.app.main import main as app_main
+from src.app.main import main as app_main
 
 
 def main(file_name):
